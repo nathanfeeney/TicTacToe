5644a5856a8819c674fe0fdf00aa11ac7182c283
Thomas Nathan Feeney <31934348+nathanfeeney@users.noreply.github.com>
2021-09-02 11:00:19 +0100

    new class

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -4,14 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows;
 
 namespace TicTacToe
 {
@@ -20,9 +13,24 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            NewGame();
+
+        }
+
+        #endregion
+
+        private void NewGame()
+        {
+            throw new NotImplementedException();
         }
     }
 }
